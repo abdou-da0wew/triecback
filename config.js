4e2741242d17d8a1da49bf283bb7accd6b3b221f
Lucifer <aabdou911aydev@gmail.com>
2025-06-08 21:19:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   prefix: process.env.PREFIX || 'i', // Or your preferred prefix
   token: process.env.TOKEN,
   mongoURI: process.env.MONGO_URI,
-  clientid: process.env.CLIENT_Id,
+  clientid: process.env.CLIENT_ID,
   backupSettings: {
     approvalChannelName: process.env.BACKUP_APPROVAL_CHANNEL || 'backup-approvals',
     defaultBackupExpiry: parseInt(process.env.BACKUP_EXPIRY_MS, 10) || 30 * 24 * 60 * 60 * 1000 // 30 a day
